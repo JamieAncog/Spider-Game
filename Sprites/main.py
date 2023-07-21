0f4cf89db3bf101614675c6bc691c9043d2aea71
Jamie Ancog <jamieancog@gmail.com>
2023-07-21 00:10:37 -0700

    fourth commit

--- Sprites/main.py
+++ Sprites/main.py
@@ -3,40 +3,88 @@ import pygame
 pygame.init()
 
 init_x = 40
-win = pygame.display.set_mode((852, 480))
+in_play = True
+win_width = 852
+win_height = 480
+win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Timing Game")
 bg = pygame.image.load("sky3.jpg")
+obstacle = pygame.image.load("smasher.png")
 walkRight = [pygame.image.load("run_right1.png"), pygame.image.load("run_right0.png"),
              pygame.image.load("run_right2.png")]
 
 
 class Player:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, vel):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 10
+        self.vel = vel
         self.right = False
         self.walkCount = 0
+        self.image = walkRight[1]
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self, window):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
 
         if self.right:
-            window.blit(walkRight[self.walkCount % 3], (self.x, self.y))
+            self.image = walkRight[self.walkCount % 3]
             self.walkCount += 1
         else:
-            window.blit(walkRight[1], (self.x, self.y))
+            self.image = walkRight[1]
+
+        window.blit(self.image, (self.x, self.y))
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
-bunny = Player(init_x, 305, 42, 105)
+class Obstacle:
+
+    def __init__(self, x, y, width, height, max_y, min_y, vel):
+        self.x = x
+        self.y = y
+        self.max_y = max_y
+        self.min_y = min_y
+        self.vel = vel
+        self.width = width
+        self.height = height
+        self.image = obstacle
+        self.rise = True
+        self.fall = False
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+
+class Entrance:
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = obstacle
+        self.rise = True
+        self.fall = False
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+
+bunny = Player(init_x, 305, 47, 116, 20)
+smasher = Obstacle(500, -10, 148, 431, -10, -220, 15)
 
 
 def redraw_game_window():
     win.blit(bg, (0, 0))
     bunny.draw(win)
+    smasher.draw(win)
     pygame.display.update()
 
 
@@ -51,7 +99,7 @@ while run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT]:
-        if bunny.x < 852 - bunny.width - bunny.vel:
+        if bunny.x < win_width - bunny.width - bunny.vel:
             bunny.x += bunny.vel
             bunny.right = True
         else:
@@ -60,6 +108,22 @@ while run:
         bunny.right = False
         bunny.walkCount = 0
 
-    redraw_game_window()
+    if smasher.y >= smasher.max_y:
+        smasher.fall = False
+        smasher.rise = True
+    elif smasher.y <= smasher.min_y:
+        smasher.rise = False
+        smasher.fall = True
+
+    if smasher.rise:
+        smasher.y -= smasher.vel
+    else:
+        smasher.y += smasher.vel
+
+    if bunny.rect.colliderect(smasher.rect):
+        in_play = False
+
+    if in_play:
+        redraw_game_window()
 
 pygame.quit()
