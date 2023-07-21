47d81fcf7b639b2a16030e872b0820f1987cfd74
Jamie Ancog <jamieancog@gmail.com>
2023-07-21 14:21:34 -0700

    seventh commit

--- Sprites/main.py
+++ Sprites/main.py
@@ -3,21 +3,26 @@ import pygame
 pygame.init()
 
 in_play = True
-win_width = 852
+win_width = 600
 win_height = 480
 border = 50
+margin = 100
+ground_height = 90
+num_smashers = 3
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Timing Game")
 bg = pygame.image.load("sky3.jpg")
 obstacle = pygame.image.load("smasher.png")
+ground = pygame.Rect(0, win_height - ground_height, win_width, ground_height)
 game_over_screen = pygame.image.load("game_over_screen.png")
-portal = pygame.image.load("portal.png")
 walkRight = [pygame.image.load("run_right1.png"), pygame.image.load("run_right0.png"),
              pygame.image.load("run_right2.png")]
 love_font = pygame.font.Font('Love.ttf', 32)
+smashers = []
 
 
 class GameText:
+
     def __init__(self, font, color, header, text_border):
         self.font = font
         self.color = color
@@ -37,9 +42,10 @@ class GameText:
 
 
 class Player:
+
     def __init__(self, x, y, width, height, vel):
         self.x = x
-        self.y = y
+        self.y = y - height
         self.width = width
         self.height = height
         self.vel = vel
@@ -58,57 +64,77 @@ class Player:
         else:
             self.image = walkRight[1]
 
-        window.blit(self.image, (self.x, self.y))
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        window.blit(self.image, (self.x, self.y))
 
 
 class Obstacle:
 
-    def __init__(self, x, y, width, height, max_y, min_y, vel):
+    def __init__(self, x, width, height, max_y, min_y, vel, acc):
         self.x = x
-        self.y = y
+        self.width = width
+        self.height = height
+        self.y = min_y
         self.max_y = max_y
         self.min_y = min_y
         self.vel = vel
-        self.width = width
-        self.height = height
+        self.acc = acc
         self.image = obstacle
         self.rise = True
         self.fall = False
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
+    def move_char(self, w_width, char):
+
+        if self.x > -w_width:
+            self.x -= char.vel
+            char.right = True
+        else:
+            self.x = win_width
+            self.vel += self.acc
+
+    def crush(self, surface_rect):
+        if self.rect.colliderect(surface_rect):
+            self.fall = False
+            self.rise = True
+        elif self.y <= self.min_y:
+            self.rise = False
+            self.fall = True
+
+        if self.rise:
+            rise_inc = self.vel
+            if self.y - self.vel < self.min_y:
+                rise_inc = self.y - self.min_y
+            self.y -= rise_inc
+        else:
+            fall_inc = self.vel
+            if self.y + self.vel > self.max_y:
+                fall_inc = self.max_y - self.y
+            self.y += fall_inc
+
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
-class Portal:
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = portal
-
-    def draw(self, window):
-        window.blit(self.image, (self.x, self.y))
-
-
-left_portal = Portal(win_width-border-59, 300, 59, 117)
-right_portal = Portal(border, 300, 59, 117)
-bunny = Player(border + left_portal.width/2, 305, 47, 116, 20)
-smasher = Obstacle(450, -240, 148, 431, -10, -240, 15)
+bunny = Player(win_width/4 - 47/2, win_height - ground_height, 47, 116, 40)
 title = GameText(love_font, (0, 0, 0), 'Bunny Game', border)
+
+for n in range(num_smashers):
+    new_smasher = Obstacle(-win_width + (win_width * 2 / num_smashers) * n, 148, 431, -25, -240, 15, 10)
+    smashers.append(new_smasher)
+
+count = num_smashers - 1
+curr_obstacle = smashers[count]
 
 
 def redraw_game_window():
     win.blit(bg, (0, 0))
     bunny.draw(win)
-    smasher.draw(win)
-    left_portal.draw(win)
-    right_portal.draw(win)
+    for obj in smashers:
+        obj.draw(win)
     title.draw(win)
+    pygame.draw.rect(win, (0, 0, 0), ground)
     pygame.display.update()
 
 
@@ -116,48 +142,48 @@ run = True
 while run:
     pygame.time.delay(100)
 
-    if bunny.rect.colliderect(smasher.rect):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    game_over = False
+
+    for smasher in smashers:
+        if bunny.rect.colliderect(smasher.rect):
+            game_over = True
+
+    if game_over:
         in_play = False
         win.blit(bg, (0, 0))
+        pygame.draw.rect(win, (0, 0, 0), ground)
         win.blit(bunny.image, (bunny.x, bunny.y))
-        win.blit(left_portal.image, (left_portal.x, left_portal.y))
-        win.blit(right_portal.image, (right_portal.x, right_portal.y))
-        win.blit(smasher.image, (smasher.x, smasher.y))
+        for smasher in smashers:
+            win.blit(smasher.image, (smasher.x, smasher.y))
         win.blit(game_over_screen, (0, 0))
         title.color = (255, 255, 255)
         title.draw(win)
         pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT] and in_play:
-        if bunny.x < win_width - 50 - right_portal.width/2 - bunny.width - bunny.vel:
-            bunny.x += bunny.vel
-            bunny.right = True
-        else:
-            bunny.x = border + left_portal.width/2
+
+        for smasher in smashers:
+            smasher.move_char(win_width, bunny)
+
+        if curr_obstacle.x == bunny.vel:
             title.score += 1
-            smasher.vel += 5
+            count += 1
+            curr_obstacle = smashers[count % num_smashers]
+
     else:
         bunny.right = False
         bunny.walkCount = 0
 
-    if smasher.y >= smasher.max_y:
-        smasher.fall = False
-        smasher.rise = True
-    elif smasher.y <= smasher.min_y:
-        smasher.rise = False
-        smasher.fall = True
-
     if in_play:
-        if smasher.rise:
-            smasher.y -= smasher.vel
-        else:
-            smasher.y += smasher.vel
+
+        for smasher in smashers:
+            smasher.crush(ground)
 
     if in_play:
         redraw_game_window()
